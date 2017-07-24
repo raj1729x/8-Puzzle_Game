48b965628f9b88be7f6ecb2a4b402d30613f1922
Raj <raj1729x@gmail.com>
2017-07-25 03:34:53 +0530

    DFS added

--- main.py
+++ main.py
@@ -8,26 +8,64 @@ search = []
 search_mother = [] 
 #just to test if it works
 search.append(initial_state)
-search_mother.append(0)
+search_mother.append(-1)
 
 #for BFS
-while True:
-    current_state = search[0]
-    if(current_state == goal):
+def BFS():
+    while True:
+        current_state = search[0]
+        if(current_state == goal):
+            explored.append(search[0])
+            exp_mother.append(search_mother[0])
+            break
+        state = Node(current_state)
+        state.Explore()
+        for i in range(0,len(state.search_space)):
+            if(state.search_space[i] not in explored):
+                search.append(state.search_space[i])
+                search_mother.append(len(explored))
         explored.append(search[0])
         exp_mother.append(search_mother[0])
-        break
-    state = Node(current_state)
-    state.Explore()
-    for i in range(0,len(state.search_space)):
-        if(state.search_space[i] not in explored):
-            search.append(state.search_space[i])
-            search_mother.append(len(explored))
-    explored.append(search[0])
-    exp_mother.append(search_mother[0])
-    del search[0]
-    del search_mother[0]
-
+        del search[0]
+        del search_mother[0]
+#for DFS
+def DFS(maxdepth):
+    x=False
+    while True:
+        if x:
+            if len(search) == 0:
+                print("Cannot find answer in this small depth")
+                break
+            flag = 0
+            temp = search_mother[len(search)-1]
+            for i in range(0,maxdepth):
+                temp = exp_mother[temp]
+                if temp == -1:
+                    flag = 1
+                    break
+            if flag == 0:
+                del search[len(search)-1]
+                del search_mother[len(search_mother)-1]
+                continue
+        x=True
+        current_state = search[len(search)-1]        
+        if(current_state == goal):
+            explored.append(search[len(search)-1])
+            exp_mother.append(search_mother[len(search)-1])
+            break
+        state = Node(current_state)
+        state.Explore()
+        explored.append(search[len(search)-1])
+        exp_mother.append(search_mother[len(search)-1])
+        del search[len(search)-1]
+        del search_mother[len(search_mother)-1]
+        for i in range(0,len(state.search_space)):
+            if(state.search_space[i] not in explored):
+                search.append(state.search_space[i])
+                search_mother.append(len(explored)-1)
+        
+DFS(20)
+#BFS()
 i=len(explored)-1
 while True:
     print(explored[i])
