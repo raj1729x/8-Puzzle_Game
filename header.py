039a26bf98076b4e4a7b15e37dca10ed68a05fdb
Raj <raj1729x@gmail.com>
2017-07-25 23:27:28 +0530

    UCS added

--- header.py
+++ header.py
@@ -1,23 +1,53 @@
-class Node:
-    goal = [0,1,2,3,4,5,6,7,8]
+goal = [0,1,2,3,4,5,6,7,8]
+explored = []
+exp_mother = []
+search = []
+search_mother = []
+path_cost = []
 
-    def __init__(self,initial_state):
-        self.initial_state = initial_state
+max_depth = [0]
+algorithm_used = ""
+
+class Node:
+    
+    def __init__(self,current_node,current_node_mother_ind):
         self.search_space = []
         self.path_cost = []
-
+        self.current_node = current_node
+        i = current_node_mother_ind
+        depth = 0
+        while True:
+            if current_node_mother_ind == -1:
+                self.current_node_mother = current_node_mother_ind
+                self.current_node_mother_ind = current_node_mother_ind
+                self.depth = 0
+                break
+            else:
+                self.current_node_mother = exp_mother[current_node_mother_ind]
+                self.current_node_mother_ind = current_node_mother_ind
+                depth = depth + 1
+                if exp_mother[i] == -1 :
+                    break
+                i = exp_mother[i]
+        self.depth = depth
+        
     def Explore(self):
-        blank = self.initial_state.index(0)
+        explored.append(self.current_node)
+        exp_mother.append(self.current_node_mother_ind)
+        if self.depth > max_depth[0] :
+            max_depth[0] = self.depth
+        
+        blank = self.current_node.index(0)
         #moves up
         if blank//3==1:
-            temp2_state = self.initial_state[:]
+            temp2_state = self.current_node[:]
             temp21 = temp2_state[blank]
             temp22 = temp2_state[blank-3]
             temp2_state[blank-3] = temp21
             temp2_state[blank] = temp22
             self.search_space.append(temp2_state)
         if blank//3==2:
-            temp_state = self.initial_state[:]
+            temp_state = self.current_node[:]
             temp1 = temp_state[blank]
             temp2 = temp_state[blank-3]
             temp_state[blank-3] = temp1
@@ -25,14 +55,14 @@ class Node:
             self.search_space.append(temp_state)
         #moves down
         if blank//3==0:
-            temp_state = self.initial_state[:]
+            temp_state = self.current_node[:]
             temp1 = temp_state[blank]
             temp2 = temp_state[blank+3]
             temp_state[blank+3] = temp1
             temp_state[blank] = temp2
             self.search_space.append(temp_state)
         if blank//3==1:
-            temp1_state = self.initial_state[:]
+            temp1_state = self.current_node[:]
             temp11 = temp1_state[blank]
             temp12 = temp1_state[blank+3]
             temp1_state[blank+3] = temp11
@@ -40,14 +70,14 @@ class Node:
             self.search_space.append(temp1_state)
         #moves left
         if blank%3==1:
-            temp2_state = self.initial_state[:]
+            temp2_state = self.current_node[:]
             temp21 = temp2_state[blank]
             temp22 = temp2_state[blank-1]
             temp2_state[blank-1] = temp21
             temp2_state[blank] = temp22
             self.search_space.append(temp2_state)
         if blank%3==2:
-            temp_state = self.initial_state[:]
+            temp_state = self.current_node[:]
             temp1 = temp_state[blank]
             temp2 = temp_state[blank-1]
             temp_state[blank-1] = temp1
@@ -55,22 +85,21 @@ class Node:
             self.search_space.append(temp_state)
         #moves right
         if blank%3==0:
-            temp_state = self.initial_state[:]
+            temp_state = self.current_node[:]
             temp1 = temp_state[blank]
             temp2 = temp_state[blank+1]
             temp_state[blank+1] = temp1
             temp_state[blank] = temp2
             self.search_space.append(temp_state)
         if blank%3==1:
-            temp1_state = self.initial_state[:]
+            temp1_state = self.current_node[:]
             temp11 = temp1_state[blank]
             temp12 = temp1_state[blank+1]
             temp1_state[blank+1] = temp11
             temp1_state[blank] = temp12
             self.search_space.append(temp1_state)
         #calculate cost for each path
-        self.Cost()
- 
+        
     def Cost(self):
         for i in range(0,len(self.search_space)):
             cost = 0
@@ -78,7 +107,7 @@ class Node:
             for j in range(0,len(temp)):
                 temp_val = temp[j]
                 temp_ind = j
-                goal_ind = self.goal.index(temp_val)
+                goal_ind = goal.index(temp_val)
                 cost = cost + abs((temp_ind%3)-(goal_ind%3)) + abs((temp_ind//3)-(goal_ind//3))
             self.path_cost.append(cost)                        
         
--- main.py
+++ main.py
@@ -1,76 +1,116 @@
-from header import *
+#initial_state = [1,4,2,3,7,5,6,8,0]
+#initial_state = [5,0,2,3,1,4,6,7,8]
+initial_state = [1,2,3,4,5,0,6,7,8]
 
-initial_state = [1,2,3,4,0,5,6,7,8]
-goal = [0,1,2,3,4,5,6,7,8]
-explored = []
-exp_mother = []
-search = []
-search_mother = [] 
+from header import *
+import time
+
 #just to test if it works
 search.append(initial_state)
 search_mother.append(-1)
 
-#for BFS
 def BFS():
+    global algorithm_used
+    algorithm_used = "BSF"
     while True:
-        current_state = search[0]
-        if(current_state == goal):
+        state = Node(search[0],search_mother[0])
+        if(state.current_node == goal):
             explored.append(search[0])
             exp_mother.append(search_mother[0])
             break
-        state = Node(current_state)
         state.Explore()
         for i in range(0,len(state.search_space)):
             if(state.search_space[i] not in explored):
                 search.append(state.search_space[i])
-                search_mother.append(len(explored))
-        explored.append(search[0])
-        exp_mother.append(search_mother[0])
+                search_mother.append(len(explored)-1)
         del search[0]
         del search_mother[0]
-#for DFS
+
 def DFS(maxdepth):
-    x=False
+    global algorithm_used
+    algorithm_used = "DSF"
     while True:
-        if x:
-            if len(search) == 0:
-                print("Cannot find answer in this small depth")
-                break
-            flag = 0
-            temp = search_mother[len(search)-1]
-            for i in range(0,maxdepth):
-                temp = exp_mother[temp]
-                if temp == -1:
-                    flag = 1
-                    break
-            if flag == 0:
-                del search[len(search)-1]
-                del search_mother[len(search_mother)-1]
-                continue
-        x=True
-        current_state = search[len(search)-1]        
-        if(current_state == goal):
+        if len(search) == 0:
+            print("Cannot find answer in this small depth")
+            break
+        state = Node(search[len(search)-1],search_mother[len(search_mother)-1])
+        if state.depth == maxdepth :
+            del search[len(search)-1]
+            del search_mother[len(search_mother)-1]
+            continue
+        if(state.current_node == goal):
             explored.append(search[len(search)-1])
             exp_mother.append(search_mother[len(search)-1])
             break
-        state = Node(current_state)
         state.Explore()
-        explored.append(search[len(search)-1])
-        exp_mother.append(search_mother[len(search)-1])
         del search[len(search)-1]
         del search_mother[len(search_mother)-1]
         for i in range(0,len(state.search_space)):
             if(state.search_space[i] not in explored):
                 search.append(state.search_space[i])
                 search_mother.append(len(explored)-1)
+
+def UCS():
+    global algorithm_used
+    algorithm_used = "UCS"
+    cost = 0
+    temp = search[0][:]
+    for j in range(0,len(temp)):
+        temp_val = temp[j]
+        temp_ind = j
+        goal_ind = goal.index(temp_val)
+        cost = cost + abs((temp_ind%3)-(goal_ind%3)) + abs((temp_ind//3)-(goal_ind//3))
+    path_cost.append(cost)
+
+    while True:
+        state = Node(search[0],search_mother[0])
+        if(state.current_node == goal):
+            explored.append(search[0])
+            exp_mother.append(search_mother[0])
+            break
+        state.Explore()
+        state.Cost()
+        for i in range(0,len(state.search_space)):
+            if(state.search_space[i] not in explored):
+                search.append(state.search_space[i])
+                path_cost.append(state.path_cost[i])
+                search_mother.append(len(explored)-1)
+        del search[0]
+        del search_mother[0]
+        del path_cost[0]
+        for i in range(0,len(path_cost)-1):
+            for j in range(i,len(path_cost)):
+                if path_cost[i] > path_cost[j]:
+                    temp1 = path_cost[i]
+                    temp2 = path_cost[j]
+                    path_cost[i] = temp2
+                    path_cost[j] = temp1
+                    temp1 = search[i]
+                    temp2 = search[j]
+                    search[i] = temp2
+                    search[j] = temp1
+                    temp1 = search_mother[i]
+                    temp2 = search_mother[j]
+                    search_mother[i] = temp2
+                    search_mother[j] = temp1
+                    
+start_time = time.time()
         
-DFS(20)
-#BFS()
+#UCS()       
+#DFS(10)
+BFS()
+
+elapse_time = time.time() - start_time
+
+print("Algorithm Used : " + str(algorithm_used))
+print("Number of Nodes explored : " + str(len(explored)))
+print("Maximum Depth Explored : " + str(max_depth[0]))
+print("Time required : " + str(elapse_time) + " seconds")
+print("Steps :")
 i=len(explored)-1
 while True:
     print(explored[i])
-    i=exp_mother[i]
     if explored[i] == initial_state:
         break
+    i=exp_mother[i]
     
-
